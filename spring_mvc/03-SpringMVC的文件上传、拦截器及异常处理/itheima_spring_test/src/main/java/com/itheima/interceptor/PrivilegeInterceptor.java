24125bb2c00537c989e44ff4f4691adf01f7ad60
CHIU TZE CHEUNG <lihaotong10@gmail.com>
2021-06-22 12:11:17 +0800

    spring mvc uploaded

---
+++ spring_mvc/03-SpringMVC的文件上传、拦截器及异常处理/itheima_spring_test/src/main/java/com/itheima/interceptor/PrivilegeInterceptor.java
@@ -0,0 +1,19 @@
+package com.itheima.interceptor;
+
+import com.itheima.domain.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PrivilegeInterceptor implements HandlerInterceptor {
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User)request.getSession().getAttribute("user");
+        if(user == null){
+            response.sendRedirect(request.getContextPath()+"/login.jsp");
+            return false;
+        }
+        return true;
+    }
+}
